69a31322e0b7fdacd74bb1827594c7f5d11bf4dd
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-12-19 18:58:48 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Ben Godfrey on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
