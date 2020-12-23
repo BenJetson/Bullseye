c2d460ad81e9fd489fad0d78d60db7fa772cf6d2
Ben Godfrey <BenJetson@users.noreply.github.com>
2020-12-22 21:41:14 -0500

    End of Part 1: Getting Started with SwiftUI.

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯\nPut the bullseye as close as you can to the target!".uppercased())
+                .bold()
+                .kerning(1.4)
+                .padding()
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+                .fontWeight(.black)
+                .kerning(-1)
+                .font(.title)
+            HStack {
+                Text("1")
+                    .bold()
+                Slider(
+                    value: .constant(50),
+                    in: 1.0 ... 100.0
+                )
+                Text("100")
+                    .bold()
+            }
+            Button(action: {}) {
+                Text("Hit Me")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
+        ContentView()
+            .previewLayout(
+                .fixed(
+                    width: 568,
+                    height: 320
+                )
+            )
     }
 }
